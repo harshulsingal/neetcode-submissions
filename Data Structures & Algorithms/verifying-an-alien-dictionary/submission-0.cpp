8f8752faaa26a5d7307a2580d3152f34be5afa18
Harshul Singal <139004390+harshulsingal@users.noreply.github.com>
2026-08-30 19:03:06 +0530

    Add: verifying-an-alien-dictionary - submission-0

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        vector<int> rank(26);
+
+        for (int i = 0; i < 26; i++) {
+            rank[order[i] - 'a'] = i;
+        }
+
+        for (int i = 0; i < words.size() - 1; i++) {
+            string a = words[i];
+            string b = words[i + 1];
+
+            int j = 0;
+
+            while (j < a.size() && j < b.size()) {
+                if (a[j] != b[j]) {
+                    if (rank[a[j] - 'a'] > rank[b[j] - 'a'])
+                        return false;
+
+                    break;
+                }
+
+                j++;
+            }
+
+            // b is a prefix of a → invalid
+            if (j == b.size() && j < a.size())
+                return false;
+        }
+
+        return true;
+    }
+};
